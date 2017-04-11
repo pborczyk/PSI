c3ba6f4da4d8ae6b5610d5beec500c31453e1d95
Piotr Borczyk <howorang@gmail.com>
2017-04-12 01:10:30 +0200

    changes

--- lab3/category.m
+++ lab3/category.m
@@ -1,29 +1,41 @@
+clearvars
 % e = y - T(n)
 % delta w = e * [x1 x2]
 % wb = wb + e
-weight_function = @(w1,w2,wb,x,y) sign([w1 w2] * [x; y] + 1 * wb);
-
+weight_function = @(w1,w2,wb,x,y) signum([w1, w2] * [x; y] + 1 * wb);
 
 PositivePoints = horzcat ([-8; 20] ,[-9; -10], [-6; -5], [2; 20], [4; 25]); 
 NegativePoints = horzcat ([-6; -25], [-2; -10], [1; -10], [4; -16], [5; 9]);
 Points = horzcat (PositivePoints, NegativePoints);
 Teacher = horzcat (ones(1, 5), zeros(1, 5));
 
-w1 = rand();
-w2 = rand();
+w(1) = rand();
+w(2) = rand();
 wb = rand();
 
 is_completed = false;
 
 while ~is_completed
     for i = 1 :  length(Points)
-       result = weight_function(w1,w2,wb,Points(1,i), Points(2,i));
+       result = weight_function(w(1),w(2),wb,Points(1,i), Points(2,i));
        if result ~= Teacher(i)
-           error = result - expected;
-           delta_w = error * [Points(1,i), Points(2,i)];
+           error =  Teacher(i) - result;
            wb = wb + error;
-           w1 = w1 + delta_w;
-           w2 = w2 + delta_w;
+           delta_w = error * [Points(1,i), Points(2,i)];
+           w = w + delta_w;
+           break;
+       end
+       
+       if i == length(Points)
+          is_completed = true; 
        end
     end
 end
+
+
+% wzór na prost¹ do sprawdzenia
+line_x = -10:0.001:10;
+line_y = (-w(1) * line_x - wb)/ -w(2)* (-1);
+
+plot(Points(1,1:5),Points(2,1:5),'*',Points(1,6:10),Points(2,6:10),'.',line_x,line_y)
+
