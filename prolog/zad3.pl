15eae5ec963aec66eab1f7217fc3e693c300e257
Piotr Broczyk <howorang@gmail.com>
2017-09-10 22:18:22 +0200

    zmiany

--- prolog/zad3.pl
+++ prolog/zad3.pl
@@ -1,12 +1,8 @@
-sportowiec(X) :- plywa(X); biega(X).
-zawody(X) :- sportowiec(X), ma_kondycje(X).
-pilkarz(X) :- biega(X), ma_kondycje(X).
-
-biega(marek).
-biega(jan).
-
-ma_kondycje(marek).
-ma_kondycje(jan).
-
-plywa(adam).
-plywa(jan).
+jest(X, sportowiec) :- jest(X, plywak); biega(X).
+zawody(X) :- jest(X, sportowiec), ma_kondycje(X).
+biega(X) :- jest(X, pilkarz).
+ma_kondycje(X) :- jest(X, pilkarz). 
+jest(marek, pilkarz).
+jest(adam, plywak).
+jest(jan, pilkarz).
+jest(jan, plywak).
